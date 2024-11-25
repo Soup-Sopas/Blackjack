4fc3b255186eb769d3f5350db8a927da78d067d1
Aquiles <aquilesrl08@gmail.com>
2024-11-25 09:23:32 +0100

    Funciones

--- Blackjack.py
+++ Blackjack.py
@@ -16,10 +16,14 @@ def crear_baraja() -> list:
     random.shuffle(baraja)
     return baraja
 
-def mostrar_cartas(cartas) -> None:
+
+def mostrar_cartas(cartas, dealer = False) -> None:
     for fila in range(4):
+        ace = 0
         for columna, carta in enumerate(cartas):
             valor, palo = carta
+            if valor == 'a': ace += 1
+            if dealer and columna == 1: valor, palo = '*', '?'
             if fila == 0:
                 print(' ___ ', end=' ')
             elif fila == 1:
@@ -29,8 +33,46 @@ def mostrar_cartas(cartas) -> None:
             elif fila == 3:
                 print(f'|__{valor}|', end=' ')
         print()
+        if fila == 3 and not dealer:
+            print(f"{obtener_valor_cartas(cartas)}".rjust(len(cartas) * 5 + (len(cartas) - 1),'-'))
+
+def obtener_valor_cartas(cartas) -> int:
+    valor = 0
+    for carta in cartas:
+        if carta[0] in ['J','Q','K']:
+            valor += 10
+        elif carta[0] == 'A':
+            if valor + 11 == 21 or valor < 11 :
+                valor += 11
+            elif valor + 11 > 21:
+                valor += 1
+        else:
+            valor += carta[0]
+    return valor
+
+def escoger_as() -> int:
+    numero_as = ''
+    while not numero_as.isnumeric():
+        numero_as = input("Tienes un As quieres que sea 1 o 11: ")
+    return int(numero_as)
+
+def pedir() -> bool:
+    decision = ''
+    while decision not in ['H','S']:
+        decision = input("H | S\n").upper()
+    return True if decision == 'H' else False
 
 
 baraja = crear_baraja()
+cartas_dealer = [baraja.pop(),baraja.pop()]
 cartas_jugador = [baraja.pop(),baraja.pop()]
+print('Dealer')
+mostrar_cartas(cartas_dealer, True)
+print('Jugador')
 mostrar_cartas(cartas_jugador)
+
+if pedir():
+    cartas_jugador.append(baraja.pop())
+
+mostrar_cartas(cartas_jugador)
+
